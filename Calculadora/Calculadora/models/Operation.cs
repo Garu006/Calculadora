13f2c0f2741b64a1bc008e5447f0856b591fb592
Garu006 <garojas@uamv.edu.ni>
2024-10-07 08:31:07 -0600

    Calculadora

---
+++ Calculadora/Calculadora/models/Operation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora.models
+{
+    internal class Operation
+    {
+        public double sum(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public double minus(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public double multiplication(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public double division(double num1, double num2)
+        {
+            try
+            {
+                return num1 / num2;
+            }catch(Exception ex)
+            {
+                MessageBox.Show("error detectado");
+               
+            }
+            return 0;
+        }
+        public double power(double num1, double num2)
+        {
+            return Math.Pow(num1, num2);
+        }
+        public double squareroot(double num1)
+        {
+            return Math.Sqrt(num1);
+        }
+        public string exitprogram()
+        {
+            return "andate treintamilvecestreintaijueputa";
+        }
+    }
+}
